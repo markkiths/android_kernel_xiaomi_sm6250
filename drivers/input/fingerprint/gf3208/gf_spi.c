darkhz/uclamp

input: fingerprint: Mark IRQs as performance critical

drivers/input/fingerprint/gf3208/gf_spi.c
@@ -366,7 +366,7 @@ static int irq_setup(struct gf_dev *gf_dev)
 
 	gf_dev->irq = gf_irq_num(gf_dev);
 	status = request_threaded_irq(gf_dev->irq, NULL, gf_irq,
-			IRQF_TRIGGER_RISING | IRQF_ONESHOT,
+			IRQF_TRIGGER_RISING | IRQF_ONESHOT | IRQF_PERF_CRITICAL,
 			"gf", gf_dev);
 
 	if (status) {

markkiths/android_kernel_xiaomi_sm6250

drivers/input/fingerprint/gf3208/gf_spi.c
@@ -366,7 +366,7 @@ static int irq_setup(struct gf_dev *gf_dev)
 
 	gf_dev->irq = gf_irq_num(gf_dev);
 	status = request_threaded_irq(gf_dev->irq, NULL, gf_irq,
-			IRQF_TRIGGER_RISING | IRQF_ONESHOT,
+			IRQF_TRIGGER_RISING | IRQF_ONESHOT | IRQF_PERF_AFFINE,
 			"gf", gf_dev);
 
 	if (status) {